029561f992fc51b308036b8162131bfc159188b6
Goob38 <gabriel@epsteinig.com>
2022-01-20 14:48:38 -0500

    Final Submission

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ def sum_of_squares(a):
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+     
 
 def test_two():
         assert sum_of_squares([3,4,5]) == 50
+        
         
 
     
